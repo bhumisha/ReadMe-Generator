528ef55117799859030c79afd41c001d467b57f4
Bhumisha Dave <bhumishadave@Bhumishas-MBP.hsd1.ca.comcast.net>
2021-02-07 09:21:07 -0800

    Intital working template

---
+++ app.js
@@ -0,0 +1,103 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+// const writeToFile = require('.src//utils/generateReadMe') ;
+const generateMarkdown = require('./src/utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: username => {
+            if (username) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub Username!');
+              return false;
+            }
+          }
+      }
+
+    ]);
+  };
+
+  //Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+  const promptProjectDetail = () => {
+    return inquirer
+    .prompt([
+      /* Pass your questions in here */
+        {
+          type:"input",
+          message :"What is your project's title? (Required)",
+          name:"title",
+          validate:title => {
+            if (title) return true;
+            else {
+              console.log("Please enter project's title!");
+              return false;
+            }
+          }
+        },
+        {
+          type:"input",
+          message :"Please write a short description about your project. (Required)",
+          name:"projectDesc",
+          validate: projectDesc => {
+            if (projectDesc) return true;
+            else {
+              console.log('You need to write a project description!');
+              return false;
+            }
+          }
+        },
+        {
+          type:"list",
+          message :"What kind of license should your project have?",
+          choices : ["MIT","Apache 2.0","GNU GPLv3","BSD 3-Clause","None","Mozilla Public License 2.0"],
+          name: "license" 
+        },
+        {
+            type:"input",
+            message :"What command should be run to install dependencies?",
+            name:"dependencies"
+        },
+        {
+          type:"input",
+          message :"What command should be run to run tests?",
+          name:"testCommand"
+        },
+        {
+          type:"input",
+          message :"What does user need to know about using the repo?",
+          name:"usage"
+        },
+        {
+          type:"checkbox",
+          message :"What does user need to know about contributing to the repo?",
+          choices : ["Submit bugs and feature requests, and help us verify as they are checked in","Review source code changes","Review the documentation and make pull requests for anything from typos to new content"],
+          name:"contribute"
+        }
+      
+    ]);
+  }
+promptUser()
+    .then(promptProjectDetail)
+    .then((data)=> {
+      let fileName = "test.md";
+      const readMeData = generateMarkdown(data);
+
+      return writeToFile(fileName, readMeData);
+    });
+  
+
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
---
+++ src/utils/generateMarkdown.js
@@ -0,0 +1,106 @@
+
+
+const randerContribute = contributeOptions => {
+  console.log(contributeOptions);
+  
+  if(contributeOptions.length > 0){
+    let contri = "";
+    for(var i=0;i<contributeOptions.length;i++){
+      contri += `* ${contributeOptions[i]}\n`
+    }
+    return contri
+     //return contributeOptions.map(element => `* ${element}\t`)
+  }
+  
+}
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+  # ${data.title} 
+ 
+  
+  
+  ## Description
+  ${data.projectDesc}
+  
+  ## Table of Contents
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+
+  ## Installation
+  To install following dependencies run the following command.
+    ${data.dependencies}
+
+  ## Usage
+  ${data.usage}
+  
+  ## License 
+  
+  
+  ## contribution
+  ${randerContribute(data.contribute)}
+  
+  ## Tests
+  To run the tests,run the following command:
+    ${data.testCommand}
+  
+  ## questions
+  
+  If you have any additional questions about the repo, open an issue or contact me directly at ${data.email}.
+  You can fine more of my work at [${data.username}](https://github.com/${data.username})
+  
+  
+
+ 
+`;
+};
+
+const licenseList = [
+  {
+    name:"MIT",
+    badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    link: "https://opensource.org/licenses/MIT",
+    noticeText:'MIT License ' +
+    'Copyright (c) [year] [fullname]'+
+    'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:' +
+    'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. ' +
+    'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
+  },
+  {
+    name:"GNU GPLv3",
+    badge: "[![License: GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    link: "https://opensource.org/licenses/MIT"
+  },
+  {
+    name:"Mozilla Public License 2.0",
+    badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    link: "https://opensource.org/licenses/MIT"
+  },
+  {
+    name:"Apache 2.0",
+    badge: "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    link: "https://opensource.org/licenses/MIT"
+  },
+  {
+    name:"BSD 3-Clause",
+    badge: "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    link: "https://opensource.org/licenses/BSD-3-Clause"
+  },
+  {
+    name:"Unlicense",
+    badge: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+    link: "http://unlicense.org/"
+  }
+
+]
+module.exports = generateMarkdown;
